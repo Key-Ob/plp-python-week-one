cac9348cb4be363930fe42f6d604cda4a7ca5101
Keith Obade <keithobade76@gmail.com>
2024-02-29 20:40:10 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+age = input("What is your age: ")
+location = input("Where are you located: ")
+print("Hello ",name,", you are ",age," years old and live in ",location,".")
